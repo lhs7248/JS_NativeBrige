e7ec997aa3b77ce965fe696f41dc8bd94e50a6c0
lhs7248 <lhs7248@126.com>
2021-02-02 09:10:18 +0800

    添加相机

---
+++ LSHybirdPage/LSHybirdPage/Views/LSCameraView.h
@@ -0,0 +1,16 @@
+//
+//  LSCameraView.h
+//  LSHybirdPage
+//
+//  Created by lhs7248 on 2021/2/1.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSCameraView : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LSHybirdPage/LSHybirdPage/Views/LSCameraView.m
@@ -0,0 +1,12 @@
+//
+//  LSCameraView.m
+//  LSHybirdPage
+//
+//  Created by lhs7248 on 2021/2/1.
+//
+
+#import "LSCameraView.h"
+
+@implementation LSCameraView
+
+@end
